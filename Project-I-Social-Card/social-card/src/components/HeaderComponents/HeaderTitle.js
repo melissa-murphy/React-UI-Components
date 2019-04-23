ba86b6a9e741ba6c87d896a9ec7d45036b741ce8
Melissa Murphy <melissa@thoughtfulmonkey.design>
2019-04-22 20:37:26 -0700

    updated font to match Lambda

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,7 +3,7 @@ import "./Header.scss";
 
 const HeaderTitle = () => {
   return (
-    <div>
+    <div className="header-title">
       <h1>Lambda School</h1>
       <h3>@LambdaSchool</h3>
       <p>**timestamp here**</p>
