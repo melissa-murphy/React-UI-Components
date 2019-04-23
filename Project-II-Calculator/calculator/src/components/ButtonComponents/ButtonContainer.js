826882613101d938682391215abae0fa0aaac56a
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-23 12:26:53 -0700

    folder tree mostly done

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Buttons.scss";
+
+const ButtonContainer = () => {
+  return (
+    <div>
+        <ClearButton />
+        <NumberButtons />
+        <MathButtons />
+    </div>
+  );
+};
